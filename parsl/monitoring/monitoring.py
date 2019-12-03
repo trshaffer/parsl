trshaffer/parsl

commit e52cbfbbe9615c0a89d02678790864e71e4526b6

Guarantee monitoring will send out at least one message (#1446)

parsl/monitoring/monitoring.py
@@ -280,17 +280,23 @@ class MonitoringHub(RepresentationMixin):
         Wrap the Parsl app with a function that will call the monitor function and point it at the correct pid when the task begins.
         """
         def wrapped(*args, **kwargs):
+            command_q = Queue(maxsize=10)
             p = Process(target=monitor,
                         args=(os.getpid(),
                               task_id,
                               monitoring_hub_url,
                               run_id,
+                              command_q,
                               logging_level,
                               sleep_dur),
                         name="Monitor-Wrapper-{}".format(task_id))
             p.start()
             try:
-                return f(*args, **kwargs)
+                try:
+                    return f(*args, **kwargs)
+                finally:
+                    command_q.put("Finished")
+                    p.join()
             finally:
                 # There's a chance of zombification if the workers are killed by some signals
                 p.terminate()
@@ -437,6 +443,7 @@ def monitor(pid,
             task_id,
             monitoring_hub_url,
             run_id,
+            command_q,
             logging_level=logging.INFO,
             sleep_dur=10):
     """Internal
@@ -447,6 +454,7 @@ def monitor(pid,
 
     import logging
     import time
+    from multiprocessing import Queue
 
     format_string = "%(asctime)s.%(msecs)03d %(name)s:%(lineno)d [%(levelname)s]  %(message)s"
     logging.basicConfig(filename='{logbase}/monitor.{task_id}.{pid}.log'.format(
@@ -524,6 +532,14 @@ def monitor(pid,
             first_msg = False
         except Exception:
             logging.exception("Exception getting the resource usage. Not sending usage to Hub", exc_info=True)
+
+        try:
+            msg = command_q.get(block=False)
+            if msg == "Finished":
+                logging.info("Received task finished message. Ending the monitoring loop now.")
+                break
+        except Queue.empty:
+            logging.debug("Have not received any message.")
 
         logging.debug("sleeping")
         time.sleep(sleep_dur)

commit 70301e361620caa862e056491573a78c27ca381b

fix mypy errors introduced by #1446

parsl/monitoring/monitoring.py
@@ -283,7 +283,7 @@ class MonitoringHub(RepresentationMixin):
         Wrap the Parsl app with a function that will call the monitor function and point it at the correct pid when the task begins.
         """
         def wrapped(*args, **kwargs):
-            command_q = Queue(maxsize=10)
+            command_q = Queue(maxsize=10) # type: Queue[Any]
             p = Process(target=monitor,
                         args=(os.getpid(),
                               task_id,
@@ -541,7 +541,7 @@ def monitor(pid,
             if msg == "Finished":
                 logging.info("Received task finished message. Ending the monitoring loop now.")
                 break
-        except Queue.empty:
+        except queue.Empty:
             logging.debug("Have not received any message.")
 
         logging.debug("sleeping")