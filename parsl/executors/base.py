trshaffer/parsl

commit 6d66cab31bda9f0f7a3c0d081ce8cf658c10643a

Formalized the executor interface more by adding certain methods that were more or less tacitly used without a formal interface. These are mostly related to error handling. (#1519)
Also added abstract classes that are used to reduce copied-and-pasted code in executors, although only implementations for the newly formalized methods are present.

Co-authored-by: Yadu Nand Babuji <yadudoc1729@gmail.com>

parsl/executors/base.py
@@ -1,7 +1,8 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 from concurrent.futures import Future
+from typing import Any, Callable, Dict, Optional
 
-from typing import Any, Callable, Optional
+from parsl.providers.provider_base import JobStatus
 
 
 class ParslExecutor(metaclass=ABCMeta):
@@ -85,6 +86,45 @@ class ParslExecutor(metaclass=ABCMeta):
         The callers of ParslExecutors need to differentiate between Executors
         and Executors wrapped in a resource provider
         """
+        pass
+
+    @abstractmethod
+    def status(self) -> Dict[Any, JobStatus]:
+        """Return the status of all jobs/blocks currently known to this executor.
+
+        :return: a dictionary mapping job ids to status strings
+        """
+        pass
+
+    @abstractmethod
+    def set_bad_state_and_fail_all(self, exception: Exception):
+        """Allows external error handlers to mark this executor as irrecoverably bad and cause
+        all tasks submitted to it now and in the future to fail. The executor is responsible
+        for checking  :method:bad_state_is_set() in the :method:submit() method and raising the
+        appropriate exception, which is available through :method:executor_exception().
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def bad_state_is_set(self) -> bool:
+        """Returns true if this executor is in an irrecoverable error state. If this method
+        returns true, :property:executor_exception should contain an exception indicating the
+        cause.
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def executor_exception(self) -> Exception:
+        """Returns an exception that indicates why this executor is in an irrecoverable state."""
+        pass
+
+    @property
+    @abstractmethod
+    def tasks(self) -> Dict[str, Future]:
+        """Contains a dictionary mapping task IDs to the corresponding Future objects for all
+        tasks that have been submitted to this executor."""
         pass
 
     @property

parsl/executors/high_throughput/executor.py
@@ -5,7 +5,7 @@ import threading
 import queue
 import pickle
 from multiprocessing import Process, Queue
-from typing import Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 import math
 
 from ipyparallel.serialize import pack_apply_message
@@ -15,8 +15,8 @@ from parsl.app.errors import RemoteExceptionWrapper
 from parsl.executors.high_throughput import zmq_pipes
 from parsl.executors.high_throughput import interchange
 from parsl.executors.errors import BadMessage, ScalingFailed, DeserializationError
-from parsl.executors.base import ParslExecutor
-from parsl.providers.provider_base import ExecutionProvider, JobStatus
+from parsl.executors.status_handling import StatusHandlingExecutor
+from parsl.providers.provider_base import ExecutionProvider
 from parsl.data_provider.staging import Staging
 from parsl.addresses import get_all_addresses
 
@@ -29,7 +29,7 @@ BUFFER_THRESHOLD = 1024 * 1024
 ITEM_THRESHOLD = 1024
 
 
-class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
+class HighThroughputExecutor(StatusHandlingExecutor, RepresentationMixin):
     """Executor designed for cluster-scale
 
     The HighThroughputExecutor system has the following components:
@@ -178,15 +178,14 @@ class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
 
         logger.debug("Initializing HighThroughputExecutor")
 
+        StatusHandlingExecutor.__init__(self, provider)
         self.label = label
         self.launch_cmd = launch_cmd
-        self.provider = provider
         self.worker_debug = worker_debug
         self.storage_access = storage_access
         self.working_dir = working_dir
         self.managed = managed
         self.blocks = {}  # type: Dict[str, str]
-        self.tasks = {}  # type: Dict[str, Future]
         self.cores_per_worker = cores_per_worker
         self.mem_per_worker = mem_per_worker
         self.max_workers = max_workers
@@ -286,8 +285,6 @@ class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
 
         self.is_alive = True
 
-        self._executor_bad_state = threading.Event()
-        self._executor_exception = None
         self._queue_management_thread = None
         self._start_queue_management_thread()
         self._start_local_queue_process()
@@ -331,7 +328,7 @@ class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
         """
         logger.debug("[MTHREAD] queue management worker starting")
 
-        while not self._executor_bad_state.is_set():
+        while not self.bad_state_is_set:
             try:
                 msgs = self.incoming_q.get(timeout=1)
 
@@ -367,14 +364,8 @@ class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
 
                         if tid == -1 and 'exception' in msg:
                             logger.warning("Executor shutting down due to exception from interchange")
-                            self._executor_exception, _ = deserialize_object(msg['exception'])
-                            logger.exception("Exception: {}".format(self._executor_exception))
-                            # Set bad state to prevent new tasks from being submitted
-                            self._executor_bad_state.set()
-                            # We set all current tasks to this exception to make sure that
-                            # this is raised in the main context.
-                            for task in self.tasks:
-                                self.tasks[task].set_exception(self._executor_exception)
+                            exception, _ = deserialize_object(msg['exception'])
+                            self.set_bad_state_and_fail_all(exception)
                             break
 
                         task_fut = self.tasks[tid]
@@ -525,8 +516,8 @@ class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
         Returns:
               Future
         """
-        if self._executor_bad_state.is_set():
-            raise self._executor_exception
+        if self.bad_state_is_set:
+            raise self.executor_exception
 
         self._task_counter += 1
         task_id = self._task_counter
@@ -611,12 +602,8 @@ class HighThroughputExecutor(ParslExecutor, RepresentationMixin):
 
         return r
 
-    def status(self) -> List[JobStatus]:
-        """Return status of all blocks."""
-
-        status = self.provider.status(list(self.blocks.values()))
-
-        return status
+    def _get_job_ids(self) -> List[Any]:
+        return list(self.blocks.values())
 
     def shutdown(self, hub=True, targets='all', block=False):
         """Shutdown the executor, including all workers and controllers.

parsl/executors/status_handling.py
@@ -1,0 +1,109 @@
+import logging
+import threading
+from abc import abstractmethod
+from concurrent.futures import Future
+from typing import List, Any, Dict
+
+from parsl.executors.base import ParslExecutor
+from parsl.providers.provider_base import JobStatus, ExecutionProvider
+
+
+logger = logging.getLogger(__name__)
+
+
+class StatusHandlingExecutor(ParslExecutor):
+    def __init__(self, provider):
+        super().__init__()
+        self._provider = provider
+        self._executor_bad_state = threading.Event()
+        self._executor_exception = None
+        self._tasks = {}  # type: Dict[str, Future]
+
+    def _make_status_dict(self, job_ids: List[Any], status_list: List[JobStatus]) -> Dict[Any, JobStatus]:
+        """Given a list of job ids and a list of corresponding status strings,
+        returns a dictionary mapping each job id to the corresponding status
+
+        :param job_ids: the list of job ids
+        :param status_list: the list of job status strings
+        :return: the resulting dictionary
+        """
+        if len(job_ids) != len(status_list):
+            raise IndexError("job id list and status string list differ in size")
+        d = {}
+        for i in range(len(job_ids)):
+            d[job_ids[i]] = status_list[i]
+
+        return d
+
+    def _set_provider(self, provider: ExecutionProvider):
+        self._provider = provider
+
+    @abstractmethod
+    def _get_job_ids(self) -> List[Any]:
+        raise NotImplementedError("Classes inheriting from StatusHandlingExecutor must implement"
+                                  "_get_job_ids()")
+
+    def status(self) -> Dict[Any, JobStatus]:
+        """Return status of all blocks."""
+
+        if self._provider:
+            job_ids = list(self._get_job_ids())
+            status = self._make_status_dict(job_ids, self._provider.status(job_ids))
+        else:
+            status = {}
+
+        return status
+
+    def set_bad_state_and_fail_all(self, exception: Exception):
+        logger.exception("Exception: {}".format(exception))
+        self._executor_exception = exception
+        # Set bad state to prevent new tasks from being submitted
+        self._executor_bad_state.set()
+        # We set all current tasks to this exception to make sure that
+        # this is raised in the main context.
+        for task in self._tasks:
+            self._tasks[task].set_exception(Exception(str(self._executor_exception)))
+
+    @property
+    def bad_state_is_set(self):
+        return self._executor_bad_state.is_set()
+
+    @property
+    def executor_exception(self):
+        return self._executor_exception
+
+    @property
+    def tasks(self) -> Dict[str, Future]:
+        return self._tasks
+
+    @property
+    def provider(self):
+        return self._provider
+
+
+class NoStatusHandlingExecutor(ParslExecutor):
+    def __init__(self):
+        super().__init__()
+        self._tasks = {}
+
+    @property
+    def bad_state_is_set(self):
+        return False
+
+    @property
+    def executor_exception(self):
+        return None
+
+    def set_bad_state_and_fail_all(self, exception: Exception):
+        pass
+
+    def status(self):
+        return {}
+
+    @property
+    def tasks(self) -> Dict[str, Future]:
+        return self._tasks
+
+    @property
+    def provider(self):
+        return self._provider

parsl/executors/low_latency/executor.py
@@ -6,7 +6,6 @@ import logging
 import threading
 import queue
 from multiprocessing import Process, Queue
-from typing import List
 
 from ipyparallel.serialize import pack_apply_message  # ,unpack_apply_message
 from ipyparallel.serialize import deserialize_object  # ,serialize_object
@@ -14,16 +13,14 @@ from ipyparallel.serialize import deserialize_object  # ,serialize_object
 from parsl.executors.low_latency import zmq_pipes
 from parsl.executors.low_latency import interchange
 from parsl.executors.errors import ScalingFailed, DeserializationError, BadMessage
-from parsl.executors.base import ParslExecutor
-from parsl.providers.provider_base import JobStatus
-
+from parsl.executors.status_handling import StatusHandlingExecutor
 from parsl.utils import RepresentationMixin
 from parsl.providers import LocalProvider
 
 logger = logging.getLogger(__name__)
 
 
-class LowLatencyExecutor(ParslExecutor, RepresentationMixin):
+class LowLatencyExecutor(StatusHandlingExecutor, RepresentationMixin):
     """
     TODO: docstring for LowLatencyExecutor
     """
@@ -44,6 +41,8 @@ class LowLatencyExecutor(ParslExecutor, RepresentationMixin):
                  managed=True
                  ):
         logger.debug("Initializing LowLatencyExecutor")
+
+        StatusHandlingExecutor.__init__(self, provider)
         self.label = label
         self.launch_cmd = launch_cmd
         self.provider = provider
@@ -54,7 +53,6 @@ class LowLatencyExecutor(ParslExecutor, RepresentationMixin):
         self.working_dir = working_dir
         self.managed = managed
         self.blocks = []
-        self.tasks = {}
         self.workers_per_node = workers_per_node
 
         self._task_counter = 0
@@ -158,7 +156,7 @@ class LowLatencyExecutor(ParslExecutor, RepresentationMixin):
         """ TODO: docstring """
         logger.debug("[MTHREAD] queue management worker starting")
 
-        while True:
+        while not self.bad_state_is_set:
             task_id, buf = self.incoming_q.get()  # TODO: why does this hang?
             msg = deserialize_object(buf)[0]
             # TODO: handle exceptions
@@ -193,6 +191,9 @@ class LowLatencyExecutor(ParslExecutor, RepresentationMixin):
 
     def submit(self, func, *args, **kwargs):
         """ TODO: docstring """
+        if self.bad_state_is_set:
+            raise self.executor_exception
+
         self._task_counter += 1
         task_id = self._task_counter
 
@@ -257,14 +258,8 @@ class LowLatencyExecutor(ParslExecutor, RepresentationMixin):
             r = self.provider.cancel(to_kill)
         return r
 
-    def status(self) -> List[JobStatus]:
-        """Return status of all blocks."""
-
-        status = []  # type: List[JobStatus]
-        if self.provider:
-            status = self.provider.status(self.blocks)
-
-        return status
+    def _get_job_ids(self):
+        return self.blocks
 
     def shutdown(self, hub=True, targets='all', block=False):
         """Shutdown the executor, including all workers and controllers.

parsl/executors/workqueue/executor.py
@@ -17,8 +17,8 @@ from ipyparallel.serialize import pack_apply_message
 from parsl.app.errors import AppFailure
 from parsl.app.errors import RemoteExceptionWrapper
 from parsl.executors.errors import ExecutorError
-from parsl.executors.base import ParslExecutor
 from parsl.data_provider.files import File
+from parsl.executors.status_handling import NoStatusHandlingExecutor
 from parsl.providers.error import OptionalModuleMissing
 from parsl.executors.workqueue import workqueue_worker
 
@@ -383,7 +383,7 @@ def WorkQueueCollectorThread(collector_queue=multiprocessing.Queue(),
     return
 
 
-class WorkQueueExecutor(ParslExecutor):
+class WorkQueueExecutor(NoStatusHandlingExecutor):
     """Executor to use Work Queue batch system
 
     The WorkQueueExecutor system utilizes the Work Queue framework to
@@ -461,6 +461,7 @@ class WorkQueueExecutor(ParslExecutor):
                  init_command="",
                  full_debug=True,
                  see_worker_output=False):
+        NoStatusHandlingExecutor.__init__(self)
         if not _work_queue_enabled:
             raise OptionalModuleMissing(['work_queue'], "WorkQueueExecutor requires the work_queue module.")
 
@@ -468,7 +469,6 @@ class WorkQueueExecutor(ParslExecutor):
         self.managed = managed
         self.task_queue = multiprocessing.Queue()
         self.collector_queue = multiprocessing.Queue()
-        self.tasks = {}
         self.port = port
         self.task_counter = -1
         self.scaling_enabled = False

parsl/executors/ipp.py
@@ -2,22 +2,20 @@ import logging
 import os
 import pathlib
 import uuid
-from typing import List
 
 from ipyparallel import Client
-from parsl.providers import LocalProvider
-from parsl.providers.provider_base import JobStatus, JobState
-from parsl.utils import RepresentationMixin
-
-from parsl.executors.base import ParslExecutor
+from parsl.providers.provider_base import JobState
 from parsl.executors.errors import ScalingFailed
 from parsl.executors.ipp_controller import Controller
+from parsl.executors.status_handling import StatusHandlingExecutor
+from parsl.providers import LocalProvider
+from parsl.utils import RepresentationMixin
 from parsl.utils import wait_for_file
 
 logger = logging.getLogger(__name__)
 
 
-class IPyParallelExecutor(ParslExecutor, RepresentationMixin):
+class IPyParallelExecutor(StatusHandlingExecutor, RepresentationMixin):
     """The IPython Parallel executor.
 
     This executor uses IPythonParallel's pilot execution system to manage multiple processes
@@ -77,7 +75,8 @@ class IPyParallelExecutor(ParslExecutor, RepresentationMixin):
                  engine_debug_level=None,
                  workers_per_node=1,
                  managed=True):
-        self.provider = provider
+
+        StatusHandlingExecutor.__init__(self, provider)
         self.label = label
         self.working_dir = working_dir
         self.controller = controller
@@ -267,15 +266,8 @@ sleep infinity
 
         return r
 
-    def status(self) -> List[JobStatus]:
-        """Returns the status of the executor via probing the execution providers."""
-        if self.provider:
-            status = self.provider.status(self.engines)
-
-        else:
-            status = []
-
-        return status
+    def _get_job_ids(self):
+        return self.engines
 
     def shutdown(self, hub=True, targets='all', block=False):
         """Shutdown the executor, including all workers and controllers.

parsl/executors/swift_t.py
@@ -14,7 +14,7 @@ import multiprocessing as mp
 from ipyparallel.serialize import pack_apply_message, unpack_apply_message
 from ipyparallel.serialize import serialize_object, deserialize_object
 
-from parsl.executors.base import ParslExecutor
+from parsl.executors.status_handling import NoStatusHandlingExecutor
 
 logger = logging.getLogger(__name__)
 
@@ -146,7 +146,7 @@ def runner(incoming_q, outgoing_q):
     logger.debug("[RUNNER] Terminating")
 
 
-class TurbineExecutor(ParslExecutor):
+class TurbineExecutor(NoStatusHandlingExecutor):
     """The Turbine executor.
 
     Bypass the Swift/T language and run on top off the Turbine engines
@@ -177,6 +177,7 @@ class TurbineExecutor(ParslExecutor):
         Trying to implement the emews model.
 
         """
+        NoStatusHandlingExecutor.__init__(self)
         logger.debug("Initializing TurbineExecutor")
         self.label = label
         self.storage_access = storage_access
@@ -196,7 +197,6 @@ class TurbineExecutor(ParslExecutor):
         self.worker = mp.Process(target=runner, args=(self.outgoing_q, self.incoming_q))
         self.worker.start()
         logger.debug("Created worker : %s", self.worker)
-        self.tasks = {}
         self._scaling_enabled = False
 
     def _queue_management_worker(self):

parsl/executors/threads.py
@@ -5,13 +5,13 @@ import concurrent.futures as cf
 
 from typing import Any, List, Optional
 
-from parsl.executors.base import ParslExecutor
+from parsl.executors.status_handling import NoStatusHandlingExecutor
 from parsl.utils import RepresentationMixin
 
 logger = logging.getLogger(__name__)
 
 
-class ThreadPoolExecutor(ParslExecutor, RepresentationMixin):
+class ThreadPoolExecutor(NoStatusHandlingExecutor, RepresentationMixin):
     """A thread-based executor.
 
     Parameters
@@ -31,6 +31,7 @@ class ThreadPoolExecutor(ParslExecutor, RepresentationMixin):
     def __init__(self, label: str = 'threads', max_threads: int = 2,
                  thread_name_prefix: str = '', storage_access: List[Any] = None,
                  working_dir: Optional[str] = None, managed: bool = True):
+        NoStatusHandlingExecutor.__init__(self)
         self.label = label
         self._scaling_enabled = False
         self.max_threads = max_threads

commit acac48dabc663421ce6c40c4dda3d7fbb0370924

fix merge errors and some int vs string type errors introduced by #1519

parsl/executors/base.py
@@ -42,7 +42,6 @@ class ParslExecutor(metaclass=ABCMeta):
     label: str
     provider: ExecutionProvider
     managed: bool
-    status: Any  # what is this? used by strategy
     outstanding: Any  # what is this? used by strategy
     working_dir: Optional[str]
     storage_access: Optional[List[Any]]
@@ -132,7 +131,7 @@ class ParslExecutor(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def set_bad_state_and_fail_all(self, exception: Exception):
+    def set_bad_state_and_fail_all(self, exception: Exception) -> None:
         """Allows external error handlers to mark this executor as irrecoverably bad and cause
         all tasks submitted to it now and in the future to fail. The executor is responsible
         for checking  :method:bad_state_is_set() in the :method:submit() method and raising the
@@ -157,7 +156,7 @@ class ParslExecutor(metaclass=ABCMeta):
 
     @property
     @abstractmethod
-    def tasks(self) -> Dict[str, Future]:
+    def tasks(self) -> Dict[int, Future]:
         """Contains a dictionary mapping task IDs to the corresponding Future objects for all
         tasks that have been submitted to this executor."""
         pass

parsl/executors/high_throughput/executor.py
@@ -522,7 +522,7 @@ class HighThroughputExecutor(StatusHandlingExecutor, RepresentationMixin):
         Returns:
               Future
         """
-        if self.bad_state.is_set:
+        if self.bad_state_is_set:
             if self.executor_exception is None:
                 raise ValueError("Executor is in bad state, but no exception recorded")
             else:

parsl/executors/status_handling.py
@@ -2,7 +2,7 @@ import logging
 import threading
 from abc import abstractmethod
 from concurrent.futures import Future
-from typing import List, Any, Dict
+from typing import List, Any, Dict, Optional
 
 from parsl.executors.base import ParslExecutor
 from parsl.providers.provider_base import JobStatus, ExecutionProvider
@@ -16,8 +16,8 @@ class StatusHandlingExecutor(ParslExecutor):
         super().__init__()
         self._provider = provider
         self._executor_bad_state = threading.Event()
-        self._executor_exception = None
-        self._tasks = {}  # type: Dict[str, Future]
+        self._executor_exception = None  # type: Optional[Exception]
+        self._tasks = {}  # type: Dict[int, Future]
 
     def _make_status_dict(self, job_ids: List[Any], status_list: List[JobStatus]) -> Dict[Any, JobStatus]:
         """Given a list of job ids and a list of corresponding status strings,
@@ -73,7 +73,7 @@ class StatusHandlingExecutor(ParslExecutor):
         return self._executor_exception
 
     @property
-    def tasks(self) -> Dict[str, Future]:
+    def tasks(self) -> Dict[int, Future]:
         return self._tasks
 
     @property
@@ -84,7 +84,7 @@ class StatusHandlingExecutor(ParslExecutor):
 class NoStatusHandlingExecutor(ParslExecutor):
     def __init__(self):
         super().__init__()
-        self._tasks = {}
+        self._tasks = {}  # type: Dict[int, Future]
 
     @property
     def bad_state_is_set(self):
@@ -101,7 +101,7 @@ class NoStatusHandlingExecutor(ParslExecutor):
         return {}
 
     @property
-    def tasks(self) -> Dict[str, Future]:
+    def tasks(self) -> Dict[int, Future]:
         return self._tasks
 
     @property